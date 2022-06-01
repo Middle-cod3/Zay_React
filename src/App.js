7af3ccb6d0ea017fec78a5a8ec7089ba29b1684f
Middle-cod3 <92102240+Middle-cod3@users.noreply.github.com>
2022-06-01 09:48:05 +0530

    zap_react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from "./components/Home";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Footer from "./components/Footer";
+import Contact from './components/Contact';
+import SingleProduct from './components/SingleProduct';
+
 
 function App() {
+    const Routing = () => {
+        return (
+          <Routes>
+            <Route exect path="/" element={<Home />} />
+            <Route  path="/about" element={<About />} />
+            <Route  path="/shop" element={<Shop />} />
+            <Route  path="/contact" element={<Contact />} />
+            <Route  path="/single_product" element={<SingleProduct />} />
+          
+          </Routes>
+        );
+      };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <Navbar />
+    <Routing />
+    <Footer />
+      </>
+    
+   
+  )
 }
 
-export default App;
+export default App
